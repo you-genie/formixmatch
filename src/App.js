c85f290ff656d87e811c5bd41e7ba6c16589ce73
Genne Chung <yujinchung863@gmail.com>
2020-05-08 19:34:59 +0900

    [#1] Main view skeleton for application

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React and you also and you OK
           </a>
         </header>
       </div>
---
+++ src/modules/views/AppBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import withStyles from '@material-ui/core/styles/';
+
+const styles = (theme) => ({
+
+});
+
+function AppBar(props) {
+    const {classes} = props;
+
+    return (<div></div>);
+}
+
+export default  withStyles(styles)(AppBar);
