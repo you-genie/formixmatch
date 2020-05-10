8f7fb5168d1d427247eeffa32f686123a4c22b33
Genne Chung <yujinchung863@gmail.com>
2020-05-10 22:42:37 +0900

    [#2] Toolbar Setup w/o profile

--- src/modules/subviews/AppBar.js
+++ src/modules/subviews/AppBar.js
@@ -1,27 +1,88 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles/';
+import {fade, makeStyles} from '@material-ui/core/styles/';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import InputBase from '@material-ui/core/InputBase';
-import SvgIcon from '@material-ui/core/SvgIcon';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import PlaceIcon from '@material-ui/icons/Place';
+import ShareIcon from '@material-ui/icons/Share';
+import RestaurantIcon from '@material-ui/icons/Restaurant';
 import Logo from '../../assets/icons/logo.svg'
 
 const useStyles = makeStyles((theme) => ({
-
+    root: {
+        margin: theme.spacing(1, 5),
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.black, 0.03),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.black, 0.06),
+        },
+        margin: theme.spacing(0, 30),
+        '&:click': {
+            margin: theme.spacing(0, 3),
+        },
+        width: '50%'
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 3),
+        height: '100%',
+        position: 'absolute',
+        display: 'flex',
+        pointerEvents: 'none',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 1),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100px',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    }
 }));
 
 function CustomAppBar(props) {
+    const classes = useStyles();
     return (
-        <AppBar elevation="0" color="transparent" position="static">
-            <ToolBar>
-                <Button
-                    startIcon={<img src={Logo} width="150px"/>} />
-            </ToolBar>
-        </AppBar>
+        <div className={classes.root}>
+            <AppBar elevation="0" color="transparent" position="static">
+                <ToolBar>
+                    <Button
+                        startIcon={<img src={Logo} width="150px"/>} />
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder={"검색..."}
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{'area-label': 'search'}} />
+                    </div>
+                    <ShareIcon />
+                    <HomeIcon />
+                    <RestaurantIcon />
+                </ToolBar>
+            </AppBar >
+        </div>
+
     );
 }
 
