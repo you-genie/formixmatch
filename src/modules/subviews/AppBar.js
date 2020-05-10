d57b2c81bb726abdab3e217f9e65793638dfa76d
Genne Chung <yujinchung863@gmail.com>
2020-05-10 17:36:01 +0900

    [#1] Appbar basic

--- src/modules/subviews/AppBar.js
+++ src/modules/subviews/AppBar.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import {withStyles} from '@material-ui/core/styles/';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const styles = (theme) => ({
 
 });
 
-function AppBar(props) {
-    const {classes} = props;
-
-    return (<div></div>);
+function CustomAppBar(props) {
+    return (
+        <AppBar position="static">
+            <ToolBar>
+                <IconButton edge="start">
+                    <MenuIcon />
+                </IconButton>
+            </ToolBar>
+        </AppBar>
+    );
 }
 
-export default  withStyles(styles)(AppBar);
+export default  withStyles(styles)(CustomAppBar);
