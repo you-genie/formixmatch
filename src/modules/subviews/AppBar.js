ad3081a060838ba5536841c2f38b77356d8045b9
Genne Chung <yujinchung863@gmail.com>
2020-05-10 21:48:52 +0900

    [#1] Added logo icon

--- src/modules/subviews/AppBar.js
+++ src/modules/subviews/AppBar.js
@@ -4,8 +4,10 @@ import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import InputBase from '@material-ui/core/InputBase';
+import SvgIcon from '@material-ui/core/SvgIcon';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
+import Logo from 'public/icons/logo.svg';
 
 const useStyles = makeStyles((theme) => ({
 
@@ -13,9 +15,10 @@ const useStyles = makeStyles((theme) => ({
 
 function CustomAppBar(props) {
     return (
-        <AppBar color="primary" position="static">
+        <AppBar elevation="0" color="transparent" position="static">
             <ToolBar>
                 <IconButton edge="start">
+                    <SvgIcon component={Logo} />
                     <MenuIcon />
                 </IconButton>
             </ToolBar>
